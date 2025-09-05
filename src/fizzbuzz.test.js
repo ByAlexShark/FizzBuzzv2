fad8d695dee8d8f4a0731ae65f118c1ae32d223d
ByAlexShark <alexanderzafiro01@gmail.com>
2025-09-04 23:24:15 -0400

    Implementación de FizzBuzz: reglas de Fizz, Buzz y FizzBuzz

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,9 +1,26 @@
 const fizzBuzz = require('../src/fizzbuzz');
 
 describe('FizzBuzz Tests', () => {
+  
   test('Devuelve "Fizz" si es múltiplo de 3', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
     expect(fizzBuzz(6)).toBe('Fizz');
     expect(fizzBuzz(9)).toBe('Fizz');
   });
+
+  test('Devuelve "Buzz" si es múltiplo de 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+    expect(fizzBuzz(10)).toBe('Buzz');
+    expect(fizzBuzz(20)).toBe('Buzz');
+  });
+
+  test('Devuelve "FizzBuzz" si es múltiplo de 3 y 5', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  test('Devuelve el número mismo si no es múltiplo de 3 ni de 5', () => {
+    expect(fizzBuzz(1)).toBe(1);
+    expect(fizzBuzz(2)).toBe(2);
+  });
+
 });
